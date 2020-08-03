93358c43109f8a29ad970d24a08f4b99f9800758
wrsxinlang <335826963@qq.com>
2020-08-03 20:05:02 +0800

    12321

--- samples/BaseMiCakeApplication/Controllers/Base/FileUploadController.cs
+++ samples/BaseMiCakeApplication/Controllers/Base/FileUploadController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BaseMiCakeApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
@@ -15,6 +17,7 @@ namespace BaseMiCakeApplication.Controllers.Base
     [Route("[controller]/[action]")]
     public class FileUploadController : ControllerBase
     {
+        public static string[] LimitPictureType = { ".PNG", ".JPG", ".JPEG", ".BMP", ".ICO" };
         private readonly string _targetFilePath = $"/UploadFile/{DateTime.Now:yyyyMMdd}/";
 
         private static IHostEnvironment _hostingEnvironment;
@@ -22,6 +25,68 @@ namespace BaseMiCakeApplication.Controllers.Base
         public FileUploadController(IHostEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
+        }
+
+        /// <summary>
+        /// 上传图片
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <returns></returns>
+        //[Authorize]
+        public ResultModel UpLoadPhoto(IFormFile formFile)
+        {
+            ResultModel code;
+            var currentPictureWithoutExtension = Path.GetFileNameWithoutExtension(formFile.FileName);
+            var currentPictureExtension = Path.GetExtension(formFile.FileName).ToUpper();
+            var path = Directory.GetCurrentDirectory() + _targetFilePath;
+            if (LimitPictureType.Contains(currentPictureExtension))
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string name = currentPictureWithoutExtension + currentPictureExtension;
+                path += name;
+                using (var fs = System.IO.File.Create(path))
+                {
+                    formFile.CopyTo(fs);
+                    //Stream 都有 Flush() 方法，
+                    //根据官方文档的说法
+                    //“使用此方法将所有信息从基础缓冲区移动到其目标或清除缓冲区，或者同时执行这两种操作”
+                    fs.Flush();
+                }
+                code = new ResultModel();
+                return code;
+            }
+            code = new ResultModel(-1, "上传失败",null);
+            return code;
+        }
+
+
+        /// <summary>
+        /// 加载图片
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public FileContentResult LoadingPhoto(string path, string name)
+        {
+            path = Directory.GetCurrentDirectory() + path + name + ".jpeg";
+            FileInfo fi = new FileInfo(path);
+            if (!fi.Exists)
+            {
+                return null;
+            }
+
+            FileStream fs = fi.OpenRead();
+            byte[] buffer = new byte[fi.Length];
+            //读取图片字节流
+            //从流中读取一个字节块，并在给定的缓冲区中写入数据。
+            fs.Read(buffer, 0, Convert.ToInt32(fi.Length));
+            var resource = File(buffer, "image/jpeg");
+            fs.Close();
+            return resource;
         }
 
 
